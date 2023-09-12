fd25e79b0ee302edfbfc575f4d2ef51247f5b76d
JoyceQu <qulecheng2004@163.com>
2023-09-12 13:30:10 -0400

    Completed Task 5

---
+++ src/Reduce.java
@@ -0,0 +1,22 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int count = 0;
+
+        while (n > 0) {
+            count ++;
+
+            // Find out if n is divisible by 2
+            boolean divisibleBy2 = n % 2 == 0;
+
+            // Do operations accordingly
+            if (divisibleBy2) {
+                n = n / 2;
+            } else {
+                n --;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
